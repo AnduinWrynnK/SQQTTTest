ad7d77ce058b153e5d10fb847c0507e6a2c5dca7
冰原猫车侠 <1254681237@qq.com>
2020-10-12 20:09:28 +0800

    no message

---
+++ SQQTTTest/SQQTTTest/Classes/CocoaPodTest.h
@@ -0,0 +1,16 @@
+//
+//  CocoaPodTest.h
+//  testForCocoapod
+//
+//  Created by xu yongpan on 2020/10/12.
+///Users/xuyongpan/Desktop/textForCocoapod/testForCocoapod/testForCocoapod/Classes
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CocoaPodTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SQQTTTest/SQQTTTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SQQTTTest
+//
+//  Created by xu yongpan on 2020/10/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
